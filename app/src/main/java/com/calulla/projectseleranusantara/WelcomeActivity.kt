84f2e028ba604dfd245f3d9c0eec8f1f9d61eeda
Bunga-calulla <bungacalulla@gmail.com>
2025-11-05 12:45:32 +0700

    Initial commit

---
+++ app/src/main/java/com/calulla/projectseleranusantara/WelcomeActivity.kt
@@ -0,0 +1,22 @@
+package com.calulla.projectseleranusantara
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+
+        val btnStart = findViewById<Button>(R.id.btnStart)
+
+        btnStart.setOnClickListener {
+            // Pindah ke halaman Login (atau Main)
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}

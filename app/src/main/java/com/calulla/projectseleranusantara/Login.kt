79442bb9cebd4547fd2d815cf3053b1636ea8b8a
Ibnu asfar z.p <asfarzaki24@gmail.com>
2025-11-25 11:06:58 +0700

    aspar update

--- app/src/main/java/com/calulla/projectseleranusantara/Login.kt
+++ app/src/main/java/com/calulla/projectseleranusantara/Login.kt
@@ -5,16 +5,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.content.Intent
 
 class Login : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnSignIn = findViewById<Button>(R.id.btnSignIn)
+
+        btnSignIn.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
         }
     }
 }
--- app/src/main/java/com/calulla/projectseleranusantara/Register.kt
+++ app/src/main/java/com/calulla/projectseleranusantara/Register.kt
@@ -1,22 +1,24 @@
 package com.calulla.projectseleranusantara
 
+import android.content.Intent
 import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
 
 class Register : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_register)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnSignUp = findViewById<Button>(R.id.btnSignUp)
+
+        btnSignUp.setOnClickListener {
+            startActivity(Intent(this, Login::class.java))
         }
     }
 }

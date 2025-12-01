c84ef37bf4108733e069b75398f828e07358e9ce
Bunga-calulla <bungacalulla@gmail.com>
2025-12-01 19:23:12 +0700

    CALULLA UPDATE

--- app/src/main/java/com/calulla/projectseleranusantara/Login.kt
+++ app/src/main/java/com/calulla/projectseleranusantara/Login.kt
@@ -1,31 +1,43 @@
-    package com.calulla.projectseleranusantara
+package com.calulla.projectseleranusantara
 
-    import android.os.Bundle
-    import androidx.activity.enableEdgeToEdge
-    import androidx.appcompat.app.AppCompatActivity
-    import androidx.core.view.ViewCompat
-    import androidx.core.view.WindowInsetsCompat
-    import android.widget.Button
-    import android.content.Intent
-    import android.widget.TextView
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import android.content.Intent
 
-    class Login : AppCompatActivity() {
+class Login : AppCompatActivity() {
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_login)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
 
-            val btnSignIn = findViewById<Button>(R.id.btnSignIn)
-            val goRegister = findViewById<TextView>(R.id.tvSignUp)   // <-- ini ID yang benar
+        val btnSignIn = findViewById<Button>(R.id.btnSignIn)
+        val goRegister = findViewById<TextView>(R.id.tvSignUp)
 
-            btnSignIn.setOnClickListener {
-                startActivity(Intent(this, MainActivity::class.java))
-                finish()
+        val etUsername = findViewById<EditText>(R.id.etUsername)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+
+        btnSignIn.setOnClickListener {
+
+            val username = etUsername.text.toString().trim()
+            val password = etPassword.text.toString().trim()
+
+            if (username.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Isi username dan password!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
             }
 
-            goRegister.setOnClickListener {
-                startActivity(Intent(this, Register::class.java))
-                finish()
-            }
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+
+        goRegister.setOnClickListener {
+            startActivity(Intent(this, Register::class.java))
+            finish()
         }
     }
+}
+
